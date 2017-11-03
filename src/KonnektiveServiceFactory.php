57b51c62cc45d24c0836230eb653e80c6788e976
Layton Everson <layton.everson@gmail.com>
2017-11-03 13:55:07 -0600

    Added factory class

---
+++ src/KonnektiveServiceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LE\KonnektiveCrmApi;
+
+/**
+ * Factory to build a new Konnektive API instance.
+ *
+ *  Pass an array for configuration with:
+ *    apiUser
+ *    apiPassword
+ *    apiUrl
+ *
+ * Class KonnektiveServiceFactory
+ * @package LE\KonnektiveCrmApi
+ */
+class KonnektiveServiceFactory
+{
+    public function __invoke($konnectiveConfig)
+    {
+        $config = KonnektiveCrmApi::createConfig(
+            $konnectiveConfig['apiUser'],
+            $konnectiveConfig['apiPassword'],
+            $konnectiveConfig['apiUrl']
+        );
+
+        return new KonnektiveCrmApi($config);
+    }
+}
